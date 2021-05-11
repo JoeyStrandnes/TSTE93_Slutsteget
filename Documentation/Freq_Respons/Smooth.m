415271156037fb2ef3fc76be67f4d6e8d6c305c3
Joey Strandnes <joey.strandnes@hotmail.com>
2021-05-11 13:47:09 +0200

    Added images for magnitude response

--- Documentation/Freq_Respons/Smooth.m
+++ Documentation/Freq_Respons/Smooth.m
@@ -3,15 +3,61 @@ clc
 clear
 close all
 
-T = readtable('50Procent-Diskant.csv');
-Y = T{1:end,2}+T{1:end,3};
-X = T{1:end,1};
+%Include all csv files.
+Table = ["20Procent-Båda.csv", "50Procent-Bas.csv", "50Procent-Diskant.csv","100Procent-Båda.csv", "100Procent-Bas.csv", "100Procent-Diskant.csv"];
 
-B = smoothdata(Y);
 
-loglog(X,Y, X,B);
-xlim([20 20000])
-legend('Freqency respons', 'Smoothed freqency respons')
-grid on
-xlabel('Hz')
-ylabel('dB')
+for i = 1: length(Table)
+    T = readtable(Table(i)); 
+    Y = T{1:end,2}+T{1:end,3};
+    X = T{1:end,1};
+
+    B = smoothdata(Y);
+
+    Max_Val = max(B);
+    B = B./Max_Val;
+
+    subplot(2,3,i);
+   
+    semilogx(X, 20*log10(B))
+    title(Table(i));
+       
+    xlim([20 20000])
+    yl = ylim;
+    ylim([yl(1), yl(2)+5]);
+    
+    %legend('Magnitude response')
+    grid on
+    xlabel('Hz')
+    ylabel('dB')
+   
+end
+
+
+for i = 1: length(Table)
+    T = readtable(Table(i)); 
+    Y = T{1:end,2}+T{1:end,3};
+    X = T{1:end,1};
+
+    B = smoothdata(Y);
+
+    Max_Val = max(B);
+    B = B./Max_Val;
+
+    figure();
+   
+    semilogx(X, 20*log10(B))
+    title(Table(i));
+    set(gcf,'name',Table(i));
+       
+    xlim([20 20000])
+    yl = ylim;
+    ylim([yl(1), yl(2)+5]);
+    
+    %legend('Magnitude response')
+    grid on
+    xlabel('Hz')
+    ylabel('dB')
+   
+end
+
